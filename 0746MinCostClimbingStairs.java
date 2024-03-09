e6e0b553f5336989a07ace35b934a3c2f451f7d0
Nil Seri <nilseri@gmail.com>
2024-03-09 20:31:07 +0300

    746. Min Cost Climbing Stairs

---
+++ 0746MinCostClimbingStairs.java
@@ -0,0 +1,12 @@
+// https://leetcode.com/problems/min-cost-climbing-stairs
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n + 1];
+
+        for (int i = 2; i <= n; i++) {
+            dp[i] = Math.min(dp[i - 2] + cost[i - 2], dp[i - 1] + cost[i - 1]);
+        }
+        return dp[n];
+    }
+}
